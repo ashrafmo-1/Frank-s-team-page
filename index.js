b9bdd9c9dbd807380f2cfa38d0081c3ad355dc96
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-06-01 13:48:33 +0300

    change slider section, add control slider & edit kinks navgationbar

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-
-    loader.classList.add("loader--hidden");
-    loader.classList.add("loader--hidden");
-
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(loader);
-    });
+var swiper = new Swiper(".card-slider", {
+    spaceBetween: 10,
+    loop: true,
+    speed: 1000,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
 });
