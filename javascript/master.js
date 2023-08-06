f3b42a96ac2970c2902b2dea412d68c2acc360e6
Ashraf Mohamed <ashrafmohamed1176@gmail.com>
2023-08-06 14:59:11 +0300

    add counter from end page

--- javascript/master.js
+++ javascript/master.js
@@ -1,12 +1,37 @@
-const allSkills = document.querySelector('.our-skills');
+const allSkills = document.querySelector('#skills');
 const allSpans = document.querySelectorAll('.our-skills .the-progress span');
+console.log(allSkills);
 
 
 window.onscroll = function() {
     if (window.scrollY >= allSkills.offsetTop) {
-        console.log("hello bro");
         allSpans.forEach((span) => {
             span.style.width = span.dataset.width;
         });
     };
 };
+
+
+
+let nums = document.querySelectorAll('.status .box .number');
+let section = document.querySelector(".status");
+let started = false;
+
+window.onscroll = function() {
+    if (window.scrollY >= section.offsetTop) {
+        if (!started) {
+            nums.forEach((num) => startCount(num));
+        }
+        started = true;
+    }
+};
+
+function startCount(el) {
+    let goal = el.dataset.goal;
+    let count = setInterval(() => {
+        el.textContent++;
+        if (el.textContent == goal) {
+            clearInterval(count);
+        }
+    }, 2000 / goal);
+}
